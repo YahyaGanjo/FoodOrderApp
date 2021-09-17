bef5ec08a4a1d6b62883f1153c99b29f1f5a976e
Yahya Ganjo <yahya.ganjo@gmail.com>
2021-09-17 16:37:59 +0200

    fetching meals

--- food-order-app/src/hooks/useFetch.js
+++ food-order-app/src/hooks/useFetch.js
@@ -4,6 +4,9 @@ const useFetch = (query) => {
   const [fetchedMeals, setFetchedMeals] = useState([]);
   useEffect(() => {
     const fetchMeals = async () => {
+      if (query === null) {
+        return;
+      }
       const response = await fetch(
         `https://api.spoonacular.com/recipes/complexSearch?query=${query}&addRecipeNutrition=true&number=5&apiKey=1c2892fae70f42eb81a47d2df5b3adfd`
       );
